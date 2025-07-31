5b113798997aeb45001407bc427c1ca8602641e7
yeremiadio <raikkonendio@gmail.com>
2025-07-31 23:45:20 +0700

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,7 @@ import { GpsModule } from './modules/gps';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        url: configService.get<string>('DATABASE_URL'), // jika menggunakan DATABASE_URL
         ssl: configService.get<boolean>('DB_SSL'),
         entities: [__dirname + '/modules/**/*.entity{.ts,.js}'],
         migrations: [__dirname + '../migrations/*{.ts,.js}'],
